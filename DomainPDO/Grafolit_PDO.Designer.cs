40a7bf6455549f456bdf5d746280f46c9d31ad2a
Boris Dolinšek <boris.dolinsek@bovianplus.si>
2020-06-12 11:53:11 +0200

    napaka velikost dolžine EmailTo

--- DomainPDO/Grafolit_PDO.Designer.cs
+++ DomainPDO/Grafolit_PDO.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Projekti\AnalizaProdaje\DatabaseWebService\DomainPDO\Grafolit_PDO.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\GitHub\DatabaseWebService\DomainPDO\Grafolit_PDO.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
